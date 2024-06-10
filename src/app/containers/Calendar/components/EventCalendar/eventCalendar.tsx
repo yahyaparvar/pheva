745c69131029db18e9d4c0bb3b2864e6b5e8ea03
Yahya Parvar <yahyaparvar1@gmail.com>
2024-06-10 23:02:55 +0330

    fixed progress

--- src/app/containers/Calendar/components/EventCalendar/eventCalendar.tsx
+++ src/app/containers/Calendar/components/EventCalendar/eventCalendar.tsx
@@ -165,101 +165,99 @@ const EventCalendar: React.FC = () => {
   const calendarEvents = events.map(parseEvents);
 
   return (
-    <>
+    <AppContainer>
       {eventsStatus === Status.LOADING && (
         <Box sx={{ width: "100%", position: "absolute", top: 0, left: 0 }}>
           <LinearProgress />
         </Box>
       )}
-      <AppContainer>
-        <CalendarWrapper>
-          <Header>
-            <PrevNextButtonWrapper>
-              <NextPrevButton onClick={handlePrev}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </NextPrevButton>
-              <NextPrevButton onClick={handleNext}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </NextPrevButton>
-              <FormattedDate>
-                {formatDate(selectedDate?.toString())}
-              </FormattedDate>
-            </PrevNextButtonWrapper>
-            <Dropdown
-              onChange={handleViewChange}
-              items={[
-                { label: "Month", value: "dayGridMonth" },
-                { label: "Week", value: "timeGridWeek" },
-                { label: "Day", value: "timeGridDay" },
+      <CalendarWrapper>
+        <Header>
+          <PrevNextButtonWrapper>
+            <NextPrevButton onClick={handlePrev}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M7.72 12.53a.75.75 0 0 1 0-1.06l7.5-7.5a.75.75 0 1 1 1.06 1.06L9.31 12l6.97 6.97a.75.75 0 1 1-1.06 1.06l-7.5-7.5Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </NextPrevButton>
+            <NextPrevButton onClick={handleNext}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M16.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L14.69 12 7.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </NextPrevButton>
+            <FormattedDate>
+              {formatDate(selectedDate?.toString())}
+            </FormattedDate>
+          </PrevNextButtonWrapper>
+          <Dropdown
+            onChange={handleViewChange}
+            items={[
+              { label: "Month", value: "dayGridMonth" },
+              { label: "Week", value: "timeGridWeek" },
+              { label: "Day", value: "timeGridDay" },
+            ]}
+          />
+        </Header>
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={animationKey}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.14, ease: "easeIn" }}
+          >
+            <FullCalendar
+              editable
+              height={"90vh"}
+              plugins={[
+                dayGridPlugin,
+                timeGridPlugin,
+                listPlugin,
+                rrulePlugin,
+                interactionPlugin,
               ]}
+              initialView={currentView}
+              initialDate={selectedDate}
+              headerToolbar={{
+                left: "",
+                center: "",
+                right: "",
+              }}
+              //@ts-ignore
+              events={calendarEvents}
+              nextDayThreshold={"01:00:00"}
+              views={{
+                timeGridWeek: {
+                  dayHeaderFormat: { weekday: "short", day: "numeric" }, // Custom format for week view
+                },
+              }}
+              dayMaxEvents={3}
+              eventTimeFormat={{
+                hour: "numeric",
+                minute: "2-digit",
+              }}
             />
-          </Header>
-          <AnimatePresence mode="wait">
-            <motion.div
-              key={animationKey}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.14, ease: "easeIn" }}
-            >
-              <FullCalendar
-                editable
-                height={"90vh"}
-                plugins={[
-                  dayGridPlugin,
-                  timeGridPlugin,
-                  listPlugin,
-                  rrulePlugin,
-                  interactionPlugin,
-                ]}
-                initialView={currentView}
-                initialDate={selectedDate}
-                headerToolbar={{
-                  left: "",
-                  center: "",
-                  right: "",
-                }}
-                //@ts-ignore
-                events={calendarEvents}
-                nextDayThreshold={"01:00:00"}
-                views={{
-                  timeGridWeek: {
-                    dayHeaderFormat: { weekday: "short", day: "numeric" }, // Custom format for week view
-                  },
-                }}
-                dayMaxEvents={3}
-                eventTimeFormat={{
-                  hour: "numeric",
-                  minute: "2-digit",
-                }}
-              />
-            </motion.div>
-          </AnimatePresence>
-        </CalendarWrapper>
-      </AppContainer>
-    </>
+          </motion.div>
+        </AnimatePresence>
+      </CalendarWrapper>
+    </AppContainer>
   );
 };
 
