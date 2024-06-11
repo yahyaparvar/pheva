e9ffec8e6f4a3b82937d0225435d68ce31658604
Yahya Parvar <yahyaparvar1@gmail.com>
2024-06-11 14:54:16 +0330

    fixed refresh token handling

--- src/service/apiClient.ts
+++ src/service/apiClient.ts
@@ -23,38 +23,6 @@ interface RefreshTokenResponse {
   expiresIn: number;
 }
 
-let isRefreshing = false;
-let refreshSubscribers: ((token: string) => void)[] = [];
-
-const subscribeTokenRefresh = (cb: (token: string) => void) => {
-  refreshSubscribers.push(cb);
-};
-
-const onRefreshed = (token: string) => {
-  refreshSubscribers.forEach((cb) => cb(token));
-  refreshSubscribers = [];
-};
-
-const refreshAccessToken = async (): Promise<string> => {
-  const refreshToken = storage.read(LocalStorageKeys.USER_INFO)?.refresh_token;
-  if (!refreshToken) {
-    throw new Error("No refresh token available");
-  }
-
-  const response = await axios.post<RefreshTokenResponse>(
-    `${baseURL}auth/refresh-token`,
-    { code: refreshToken }
-  );
-
-  const { accessToken } = response.data;
-  storage.write(LocalStorageKeys.USER_INFO, {
-    ...storage.read(LocalStorageKeys.USER_INFO),
-    access_token: accessToken,
-  });
-  window.location.reload();
-  return accessToken;
-};
-
 export const axiosInstance: AxiosInstance = axios.create({
   baseURL,
 });
@@ -62,9 +30,15 @@ export const axiosInstance: AxiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
     const token = storage.read(LocalStorageKeys.USER_INFO)?.access_token;
+    const refreshToken = storage.read(
+      LocalStorageKeys.USER_INFO
+    )?.refresh_token;
     if (token) {
       config.headers = config.headers || {};
       config.headers.Authorization = `Bearer ${token}`;
+    }
+    if (refreshToken) {
+      config.headers["x-refresh-token"] = refreshToken;
     }
     return config;
   },
@@ -75,32 +49,16 @@ axiosInstance.interceptors.request.use(
 
 axiosInstance.interceptors.response.use(
   (response: AxiosResponse) => {
-    return response;
-  },
-  async (error: AxiosError) => {
-    const originalRequest = error.config as InternalAxiosRequestConfig;
-
-    if (error.response && error.response.status === 401) {
-      if (!isRefreshing) {
-        isRefreshing = true;
-        try {
-          const newToken = await refreshAccessToken();
-          isRefreshing = false;
-          onRefreshed(newToken);
-        } catch (refreshError) {
-          isRefreshing = false;
-          return Promise.reject(refreshError);
-        }
-      }
-
-      return new Promise((resolve) => {
-        subscribeTokenRefresh((token: string) => {
-          originalRequest.headers.Authorization = `Bearer ${token}`;
-          resolve(axios(originalRequest));
-        });
+    // Update the access token if a new one is provided by the backend
+    if (response.data.newAccessToken) {
+      storage.write(LocalStorageKeys.USER_INFO, {
+        ...storage.read(LocalStorageKeys.USER_INFO),
+        access_token: response.data.newAccessToken,
       });
     }
-
+    return response;
+  },
+  (error: AxiosError) => {
     return Promise.reject(error);
   }
 );
