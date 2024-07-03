a0c4796d70b5a189858500bc0ab4852b63717605
Yahya Parvar <yahyaparvar1@gmail.com>
2024-07-03 13:06:41 +0330

    removed test

--- src/app/containers/Home/index.tsx
+++ src/app/containers/Home/index.tsx
@@ -64,9 +64,6 @@ export function Home(props: Props) {
   }, []);
   return (
     <MotionBox>
-      <a href="/privacy-policy.pdf" target="_blank" rel="noopener noreferrer">
-        privacy & policy
-      </a>
       <Wrapper>
         <Helmet>
           <title>Home</title>
